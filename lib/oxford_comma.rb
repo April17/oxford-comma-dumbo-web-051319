9d8c5840c917ec287b184072a7ffdc5914d616ae
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 20:08:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,13 +4,15 @@ def oxford_comma(array)
   string = ""
   if array.length == 1
     return array.join
+  elsif array.length == 2
+    return array.join(" and ")
   else
     for i in 0...array.length
       if i == array.length - 1
         ary = []
         ary << string
         ary << array[i]
-        return ary.join(" and ")
+        return ary.join(", and ")
       else
         ary << array[i]
         string = ary.join(", ")
