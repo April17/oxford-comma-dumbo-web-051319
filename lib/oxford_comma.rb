cbb9deac60f9abd70117a93a390a50276fecad11
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 19:47:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  ary = []
+  if array.length == 1
+    return array.join
+  else
+    for i in 0...array.length
+      if i == array.length+1
+        ary << array[i]
+        ary.join(" and ")
+      else
+        ary << arry[i]
+        ary.join(", ")
+      end
+    end
+    return ary
 end
